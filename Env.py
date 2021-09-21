d745dce9e058511bdd7252ab1479b431cbed5773
David Ayer <davidayer.22@gmail.com>
2021-09-21 10:58:35 -0400

    Use XferEnvRecord in __init__() and init_EnvTable(), added add2CorpsEnvTable()

--- Env.py
+++ Env.py
@@ -1,6 +1,6 @@
 
 '''
-    Env Conc
+    Env Conc (aka the EnvMgr)
 
 
     Cooperate with CorpsMgr to establish the run-time environment for a Corps
@@ -10,8 +10,9 @@
 
 from Conc import Conc
 from ConcAddr import ConcAddr
-from EnvGlobals import TheThread, NullConcAddr, set_EnvId, my_EnvId, set_EnvMgrAddr, _ConcIdMgr, _Addr2Conc
-from EnvGlobals import _EnvTable, DefaultEnvRecord, set_MyPort, set_EnvStatus
+from EnvGlobals import TheThread, NullConcAddr, set_EnvId, my_EnvId, set_EnvMgrAddr, _ConcIdMgr, _Addr2Conc, \
+    _EnvTable, DefaultEnvRecord, DefaultCorpsEnvRecord, _ExtCorpsEnvTable, _ContCorpsEnvTable, set_MyPort, \
+    set_EnvStatus
 from os import getpid, kill
 from importlib import import_module
 from EnvAddrSpace import CORPSMGR_ENVID
@@ -23,11 +24,14 @@ from signal import SIGTERM
 from Config import load_config
 from logging import basicConfig, info
 from CorpsStatus import MajorStatus, MinorStatus
+from EnvRecord import XferEnvRecord, CorpsEnvRecord
 
 
 
 class Env(Conc):
     def __init__(self, EnvId, CorpsMgrQueue, ConfigFiles):
+        #from time import perf_counter
+        #t1 = perf_counter()
         load_config('ConfigGlobals', ConfigFiles)
 
         from ConfigGlobals import Logging_Format, Logging_Datefmt, Logging_Level
@@ -65,16 +69,19 @@ class Env(Conc):
         set_MyPort(Port)        # another EnvGlobal
         _EnvTable.register(EnvId, TheEnvRecord)
 
-        # If we are not the CorpsMgr's Env we need to provide the CorpsMgr our
-        #   EnvRecord Spec Tuple: 0: LocEnvId, 1: IpAddr, 2: Port
+        # If we are not the CorpsMgr's Env we need to provide the CorpsMgr our XferEnvRecord
         if EnvId != CORPSMGR_ENVID:
-            CorpsMgrQueue.put([EnvId, TheEnvRecord.IpAddr, TheEnvRecord.Port])
+            OurXferEnvRecord = XferEnvRecord(EnvId, TheEnvRecord.IpAddr, TheEnvRecord.Port)
+            CorpsMgrQueue.put(OurXferEnvRecord)
 
         # The thread is no longer assigned, so cleanup
         TheThread.TheConcAddr = NullConcAddr
 
         set_EnvStatus(MajorStatus.Running)
         info(f'EnvMgr {self.my_Name()} Env {EnvId} initialized')
+
+        #t2 = perf_counter()
+        #print(f'EnvMgr {EnvId} init: in {t2-t1} seconds')
 
 
     def __kill__(self):
@@ -86,24 +93,35 @@ class Env(Conc):
         set_EnvStatus(MajorStatus.Nonexistent)
 
 
-    def init_EnvTable(self, AllEnvMsgList):
+    def init_EnvTable(self, XerEnvRecordList):
         '''
-            Init our EnvTable using a list of EnvRecord Spec Tuples: 0: LocEnvId, 1: IpAddr, 2: Port
+            Init our EnvTable using a list of XferEnvRecords
 
             Ignore for our own which we did in __init__
 
             For now use defaults for MsgHdlrFactory and NetwHdlrFactory
         '''
 
-        for EnvMsgList in AllEnvMsgList: # 0: LocEnvId, 1: IpAddr, 2: Port
-            EnvId = EnvMsgList[0]
+        for anXferEnvRecord in XerEnvRecordList:
+            EnvId = anXferEnvRecord.LocEnvId
             if EnvId != my_EnvId():
                 TheEnvRecord = copy(DefaultEnvRecord)
-                TheEnvRecord.IpAddr = EnvMsgList[1]
-                TheEnvRecord.Port = EnvMsgList[2]
+                TheEnvRecord.IpAddr = anXferEnvRecord.IpAddr
+                TheEnvRecord.Port = anXferEnvRecord.Port
                 _EnvTable.register(EnvId, TheEnvRecord)
 
         info(f'EnvMgr {self.my_Name()} Env {my_EnvId()} EnvTable:\n{_EnvTable}')
+
+        return True
+
+
+    def add2_CorpsEnvTable(self, CorpsEnvId, Ext, NewXferCorpsEnvRecord):
+        NewCorpsEnvRecord = CorpsEnvRecord(NewXferCorpsEnvRecord.Tag, DefaultCorpsEnvRecord.MsgHdlrFactory, \
+                        DefaultCorpsEnvRecord.NetwFactory, NewXferCorpsEnvRecord.IpAddr, NewXferCorpsEnvRecord.Port)
+        if Ext == True:
+            NewCorps_EnvId = _ExtCorpsEnvTable.register(CorpsEnvId, NewCorpsEnvRecord)
+        else:
+            NewCorps_EnvId = _ContCorpsEnvTable.register(CorpsEnvId, NewCorpsEnvRecord)
 
         return True
 
