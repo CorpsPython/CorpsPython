bcf39169e1108f9bb15c3589993d601432dcb406
David Ayer <davidayer.22@gmail.com>
2021-09-21 10:58:08 -0400

    Added create_Corps(), Use XferEnvRecord in __init__() and create_Corps()

--- Corps.py
+++ Corps.py
@@ -18,7 +18,6 @@
     m y _ N a m e ( )
 
     Returns a new Name of the Corps instance.
-
 '''
 
 '''
@@ -32,9 +31,10 @@
 
 from Conc import Conc
 from ConcAddr import ConcAddr, ExtAddr
-from EnvRecord import EnvRecord
+from EnvRecord import EnvRecord, XferEnvRecord, CorpsEnvRecord, XferCorpsEnvRecord
 from Env import Env, EnvName    # EnvName is dynamically genned by proxy()
-from EnvGlobals import TheThread, NullConcAddr, _ConcIdMgr, _Addr2Conc, DefaultEnvRecord, _EnvTable,  my_Ip, my_Port
+from EnvGlobals import TheThread, NullConcAddr, _ConcIdMgr, _Addr2Conc, DefaultEnvRecord, _EnvTable,  my_Ip, my_Port, \
+    DefaultCorpsEnvRecord, _ExtCorpsEnvTable, _ContCorpsEnvTable
 from os import getpid, kill
 from multiprocessing import Process, Queue
 from EnvAddrSpace import MIN_ENVID, MIN_ENVID_plus_1, MAX_ENVID, CORPSMGR_ENVID
@@ -43,25 +43,30 @@ from signal import SIGTERM
 from logging import info
 from CorpsStatus import MajorStatus, MinorStatus
 import Workers
-
+import time
+from sys import exc_info
 
 
 
 def EnvIdGen(MinEnvId, MaxEnvId):
+    ''' Generator for EnvIds given a range of EnvIds '''
     for EnvId in range(MaxEnvId - MinEnvId + 1):
         yield EnvId + MinEnvId
 
 
 def EnvNameGen(MinEnvId, MaxEnvId):
+    ''' Generator for EnvMgr Names, given a range of EnvIds '''
     for EnvId in range(MaxEnvId - MinEnvId + 1):
         yield EnvName(ConcAddr(CORPSMGR_ENVID, ENVMGR_CONCID, EnvId + MinEnvId))
 
 
 class CorpsMgr(Conc):
     def __init__(self, ConfigFiles):
+        ''' Init a Corps, including all of its initial Envs (except its own - Corps Conc has already booted Env 0). '''
+
         # Import the Config data here to make sure we get the version loaded by EnvMgr
         from ConfigGlobals import NumEnvs
-
+        print(f'CorpsMgr starting with {NumEnvs} Envs')
         assert NumEnvs < MAX_ENVID-MIN_ENVID+1, f'Number of Envs must be less than {MAX_ENVID-MIN_ENVID+1}'
 
         # Self-initialize
@@ -78,27 +83,41 @@ class CorpsMgr(Conc):
         # Make sure we can be found
         _Addr2Conc.register(self, self.ConcAddr)
 
-        # Build a list of Envs' list of EnvRecord Spec Tuple: 0: LocEnvId, 1: IpAddr, 2: Port
+        # todo: make tuple into a named tuple or a data class?
+        # Build a list of Envs' list of EnvRecord specs with indexes: 0: LocEnvId, 1: IpAddr, 2: Port
         # Ours first, the other Envs as we receive them off the Queue
-        EnvRecordSpecs = []
+        XferEnvRecords = []
         OurEnvRecord = _EnvTable.get(CORPSMGR_ENVID)
-        EnvRecordSpecs.append([CORPSMGR_ENVID, OurEnvRecord.IpAddr, OurEnvRecord.Port])
+        OurXferEnvRecord = XferEnvRecord(CORPSMGR_ENVID, OurEnvRecord.IpAddr, OurEnvRecord.Port)
+        XferEnvRecords.append(OurXferEnvRecord)
 
         # Boot all other Envs in this Corps and get their EnvRecord Spec
         # ...Everything is on this Host
-        CorpsMgrQueue = Queue()
+        #from time import perf_counter
         EnvIds = EnvIdGen(MIN_ENVID_plus_1, NumEnvs-1)
+        #tot_t = 0
         for NewEnvId in EnvIds:
+            #t1 = perf_counter()
+            CorpsMgrQueue = Queue()
             NewEnv = Process(target=Env, args=(NewEnvId, CorpsMgrQueue, ConfigFiles))
+
             NewEnv.start()
 
-            EnvMsgList = CorpsMgrQueue.get()
-            EnvRecordSpecs.append(EnvMsgList)
+            NewXferEnvRecord = CorpsMgrQueue.get()
+            #t2 = perf_counter()
+            #tot_t += (t2 - t1)
+            #print(f'CorpsMgr init Env {NewEnvId} in {t2-t1} seconds')
+
+            assert NewXferEnvRecord.LocEnvId == NewEnvId, \
+                                f'CorpsMgr __init__ returned Envid {NewXferEnvRecord.LocEnvId} vs expected {NewEnvId}'
+            XferEnvRecords.append(NewXferEnvRecord)
 
             # Create an EnvRecord and add to our EnvTable
-            NewEnvRecord = EnvRecord(DefaultEnvRecord.MsgHdlrFactory, DefaultEnvRecord.NetwFactory,
-                                    EnvMsgList[1], EnvMsgList[2])
+            NewEnvRecord = EnvRecord(DefaultEnvRecord.MsgHdlrFactory, DefaultEnvRecord.NetwFactory, \
+                                                                        NewXferEnvRecord.IpAddr, NewXferEnvRecord.Port)
             _EnvTable.register(NewEnvId, NewEnvRecord)
+
+        #print(f'CorpsMgr {self.ConcAddr} init: queue get {NumEnvs-1} Env processes in {tot_t} seconds')
 
         info(f'CorpsMgr {self.my_Name()} EnvTable:\n{_EnvTable}')
 
@@ -106,13 +125,11 @@ class CorpsMgr(Conc):
         EnvNames = EnvNameGen(MIN_ENVID_plus_1, NumEnvs-1)
         FutRets = []
         for RemoteEnv in EnvNames:
-            FutRets.append(RemoteEnv.init_EnvTable(EnvRecordSpecs))
+            FutRets.append(RemoteEnv.init_EnvTable(XferEnvRecords))
 
+        # todo: handle the exception
         for FutRet in FutRets:
-            Ret = FutRet.Ret
-
-            assert Ret == True,\
-                f'CorpsMgr {self.my_Name()} Process {getpid()} error initing EnvTable in Env {RemoteEnv.___target___}'
+            Ret = FutRet.Ret    # will blow up if we have an exception
 
         # We're up!
         self.MajorStatus = MajorStatus.Running
@@ -122,7 +139,7 @@ class CorpsMgr(Conc):
         TheThread.TheConcAddr = NullConcAddr
 
 
-    def create_Corps(self, ModuleName, CorpsClassName, Mgr, is_Ext, Managed, LocType, LocVal, *args, **kwargs):
+    def create_Corps(self, ModuleName, CorpsClassName, Mgr, CorpsTag, is_Ext, Managed, LocType, LocVal, *args, **kwargs):
         WorkerQueue = Queue()
 
         NewCorpsProcess = Process(target=Workers.__other_process_create_Corps__, \
@@ -130,9 +147,35 @@ class CorpsMgr(Conc):
         NewCorpsProcess.start()
 
         NewCorpsData = WorkerQueue.get()
-        NewCorpsIp = NewCorpsData[0]
-        NewCorpsPort = NewCorpsData[1]
-        return (NewCorpsIp, NewCorpsPort)
+        NewCorps_Ip = NewCorpsData[0]
+        NewCorps_Port = NewCorpsData[1]
+
+        NewCorpsEnvRecord = CorpsEnvRecord(CorpsTag, DefaultEnvRecord.MsgHdlrFactory, DefaultEnvRecord.NetwFactory, \
+                                                                                            NewCorps_Ip, NewCorps_Port)
+
+        NewCorps_EnvId = CORPSMGR_ENVID
+
+        if Managed == True:
+            if is_Ext == True:
+                NewCorps_EnvId = _ExtCorpsEnvTable.register(None, NewCorpsEnvRecord)
+            else:
+                NewCorps_EnvId = _ContCorpsEnvTable.register(None, NewCorpsEnvRecord)
+
+        NewXferCorpsEnvRecord = XferCorpsEnvRecord(CorpsTag, NewCorps_EnvId, NewCorps_Ip, NewCorps_Port)
+
+        # Add new Corps to all Envs except ours
+        if Managed == True:
+            NumEnvs = _EnvTable.num_Envs()  # assumes EnvIds are sequential (not true for ContCorps and ExtCorps)
+            EnvNames = EnvNameGen(MIN_ENVID_plus_1, NumEnvs-1)
+            FutRets = []
+            for RemoteEnv in EnvNames:
+                FutRets.append(RemoteEnv.add2_CorpsEnvTable(NewCorps_EnvId, is_Ext, NewXferCorpsEnvRecord))
+
+            for FutRet in FutRets:
+                Ret = FutRet.Ret  # will blow up if we have an exception
+
+
+        return NewXferCorpsEnvRecord
 
 
     def __kill__(self):
@@ -140,11 +183,15 @@ class CorpsMgr(Conc):
         self.MajorStatus = MajorStatus.Exiting
         info(f'CorpsMgr {self.my_Name()} exiting')
 
+        # todo: kill all Corps we are managing
+
         # Request exit for all Envs except ours
         NumEnvs = _EnvTable.num_Envs()  # assumes EnvIds are sequential (not true for ContCorps and ExtCorps)
         EnvNames = EnvNameGen(MIN_ENVID_plus_1, NumEnvs-1)
         for RemoteEnv in EnvNames:
             RemoteEnv.__kill__()
+
+        time.sleep(0.1 * NumEnvs)
 
         # Now us
         kill(pid, 0)
@@ -164,6 +211,8 @@ class Corps(Conc):
         ConcId = _ConcIdMgr.new()
         assert ConcId == CORPS_CONCID, f'Corps has ConcId {ConcId} and not {CORPS_CONCID}'
 
+        # todo: add test and assign for contcorps
+        # todo: always use ExtAddrs?  assume short-lived and used to pass parms?  ok between Concs in same Corps?
         # For ContCorps
         #self.ConcAddr = ConcAddr(CORPSMGR_ENVID, ConcId, CORPSMGR_ENVID)
         # For ExtCorps
@@ -176,26 +225,31 @@ class Corps(Conc):
         # Make sure we can be found
         _Addr2Conc.register(self, self.ConcAddr)
 
-        #info(f'Corps {self.my_Name()} initialized, starting...')
-        print(f'Corps {self} __init__ running, starting...')
+        info(f'Corps Mgr {self.my_Tag()} initialized, starting...')
 
         # The thread is no longer assigned, so cleanup
         TheThread.TheConcAddr = NullConcAddr
 
 
     def __kill__(self):
+        info(f'Corps Mgr in {self.my_Tag()} exiting')
+
         # Send kill request to CorpsMgr
         CorpsMgrConcAddr = ConcAddr(CORPSMGR_ENVID, CORPSMGR_CONCID, CORPSMGR_ENVID)
         CorpsMgrConc = CorpsMgrName(CorpsMgrConcAddr)
+
         CorpsMgrConc.__kill__()
 
 
     def cleanup(self):
-        pass
+        return True
 
 
     def exit(self):
         self.cleanup()
+
+        time.sleep(0.1)
+
         self.__kill__()
 
 
